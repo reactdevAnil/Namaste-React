d7f062e7a2e91ac0ea9e41b3c8d51b6c5865b9d1
Anil <sj.anilsahoo@gmail.com>
2023-03-23 01:09:59 +0530

    Session 2 Commits

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-const heading = React.createElement("h1", {
+import React from 'react'
+import ReactDOM  from 'react-dom/client';
+
+
+const heading1 = React.createElement("h1",{},"This is Heading 1")
+const heading2 = React.createElement("h2",{},"This is Heading 2")
+
+const heading = React.createElement("div", {
     id: "headings", xyz: "xyz"
-}, "Hello World from React!");
+}, [heading1, heading2]);
 console.log(heading)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
